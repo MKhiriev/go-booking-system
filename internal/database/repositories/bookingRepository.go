c1d04d60ef9610337d78f9769558d869fb724bb6
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-04-04 08:42:24 +0500

    Implemented all methods from BookingRepository Interface

--- internal/database/repositories/bookingRepository.go
+++ internal/database/repositories/bookingRepository.go
@@ -1,23 +1,75 @@
 package repositories
 
 import (
+	"database/sql"
+	"errors"
 	"gorm.io/gorm"
 	"humoBooking/internal/models"
+	"log"
 	"time"
 )
 
+// BookingRepository TODO create custom errors
 type BookingRepository struct {
 	connection *gorm.DB
 }
 
 func (b *BookingRepository) Update(booking models.Booking) (models.Booking, error) {
-	//TODO implement me
-	panic("implement me")
+	// checks if Overlapping bookings exists
+	overlappingBookings, err := b.GetBookingsByRoomIdAndBookingTime(
+		booking.RoomId, booking.DateTimeStart, booking.DateTimeEnd)
+	if err != nil {
+		log.Println("BookingRepository.GetBookingsByRoomIdAndBookingTime(): error occured during Booking creation. Passed data: ",
+			booking.RoomId, booking.DateTimeStart, booking.DateTimeEnd)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+	if len(overlappingBookings) == 1 && overlappingBookings[0].BookingId == booking.BookingId {
+		log.Println("everything is cool")
+	} else if len(overlappingBookings) > 0 {
+		log.Println("Booking cannot be updated. Reason: overlaps with existing bookings")
+		for _, overlappingBooking := range overlappingBookings {
+			log.Println(overlappingBooking)
+		}
+		return models.Booking{}, errors.New("booking cannot be updated. Reason: overlaps with existing bookings")
+	}
+
+	result := b.connection.
+		Omit("active", "created_at", "deleted_at"). // `active` is changed only at DELETION
+		Model(&booking).
+		Updates(&booking)
+
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.Update(): error occured during Booking update. Passed data: ", booking)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+
+	return booking, nil
 }
 
 func (b *BookingRepository) Delete(bookingId int) (bool, error) {
-	//TODO implement me
-	panic("implement me")
+	bookingToDelete := models.Booking{
+		BookingId: bookingId,
+		Active:    false,
+		DeletedAt: time.Now(),
+	}
+
+	result := b.connection.
+		Select("*").
+		Omit("created_at", "updated_at", "room_id", "user_id", "datetime_start", "datetime_end").
+		Model(&bookingToDelete).
+		Updates(&bookingToDelete)
+
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.Delete(): error occured during Booking deletion. Passed data: ", bookingId)
+		log.Println(err)
+		return false, err
+	}
+
+	log.Println(bookingToDelete)
+
+	return true, nil
 }
 
 func NewBookingRepositoryPostgres(connection *gorm.DB) *BookingRepository {
@@ -25,43 +77,108 @@ func NewBookingRepositoryPostgres(connection *gorm.DB) *BookingRepository {
 }
 
 func (b *BookingRepository) Create(booking models.Booking) (models.Booking, error) {
-	//TODO implement me
-	panic("implement me")
+	// checks if Overlapping bookingsa exists
+	overlappingBookings, err := b.GetBookingsByRoomIdAndBookingTime(
+		booking.RoomId, booking.DateTimeStart, booking.DateTimeEnd)
+	if err != nil {
+		log.Println("BookingRepository.GetBookingsByRoomIdAndBookingTime(): error occured during Booking creation. Passed data: ",
+			booking.RoomId, booking.DateTimeStart, booking.DateTimeEnd)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+	if len(overlappingBookings) > 0 {
+		log.Println("Booking cannot be created. Reason: overlaps with existing bookings")
+		for _, overlappingBooking := range overlappingBookings {
+			log.Println(overlappingBooking)
+		}
+		return models.Booking{}, errors.New("booking cannot be updated. Reason: overlaps with existing bookings")
+	}
+
+	result := b.connection.
+		Omit("updated_at", "deleted_at", "active").
+		Create(&booking)
+
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.Create(): error occured during Booking creation. Passed data: ", booking)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+
+	return booking, nil
 }
 
 func (b *BookingRepository) GetAll() []models.Booking {
-	//TODO implement me
-	panic("implement me")
+	var allBookings []models.Booking
+
+	b.connection.Find(&allBookings)
+
+	return allBookings
 }
 
 func (b *BookingRepository) GetBookingById(bookingId int) (models.Booking, error) {
-	//TODO implement me
-	panic("implement me")
+	var foundBooking models.Booking
+
+	result := b.connection.Find(&foundBooking, "booking_id", bookingId)
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.GetRoomById(): error occured during Booking search. Passed data: ", bookingId)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+
+	return foundBooking, nil
 }
 
 func (b *BookingRepository) GetBookingsByRoomId(roomId int) ([]models.Booking, error) {
-	//TODO implement me
-	panic("implement me")
+	var foundBookings []models.Booking
+
+	result := b.connection.Find(&foundBookings, "room_id", roomId)
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.GetBookingsByRoomId(): error occured during Bookings search. Passed data: ", roomId)
+		log.Println(err)
+		return nil, err
+	}
+
+	return foundBookings, nil
 }
 
 func (b *BookingRepository) GetBookingsByRoomIdAndBookingTime(roomId int, dateTimeStart time.Time, dateTimeEnd time.Time) ([]models.Booking, error) {
-	/*
-		SELECT *
-		FROM Booking
-		WHERE room_id = @room_id
-		AND (
+	var overlapingBookings []models.Booking
+
+	result := b.connection.
+		Where("room_id = @room_id", sql.Named("room_id", roomId)).
+		Where(`(
 		    (@datetime_start BETWEEN datetime_start AND datetime_end)
 		    OR (@datetime_end BETWEEN datetime_start AND datetime_end)
 		    OR (datetime_start BETWEEN @datetime_start AND @datetime_end)
-		    OR (datetime_end BETWEEN @datetime_start AND @datetime_end)
-		);
-	*/
+		    OR (datetime_end BETWEEN @datetime_start AND @datetime_end))`,
+			sql.Named("datetime_start", dateTimeStart),
+			sql.Named("datetime_end", dateTimeEnd)).
+		Find(&overlapingBookings)
 
-	//TODO implement me
-	panic("implement me")
+	if err := result.Error; err != nil {
+		log.Println("BookingRepository.GetBookingsByRoomIdAndBookingTime(): error occured during overlaping Booking search. Passed data: ", roomId, dateTimeStart, dateTimeEnd)
+		log.Println(err)
+		return nil, err
+	}
+
+	return overlapingBookings, nil
 }
 
+// BookRoom Probably Service level...
 func (b *BookingRepository) BookRoom(userId int, roomId int, dateTimeStart time.Time, dateTimeEnd time.Time) (models.Booking, error) {
-	//TODO implement me
-	panic("implement me")
+	requestedBooking := models.Booking{
+		UserId:        userId,
+		RoomId:        roomId,
+		DateTimeStart: dateTimeStart,
+		DateTimeEnd:   dateTimeEnd,
+	}
+
+	booking, err := b.Create(requestedBooking)
+	if err != nil {
+		log.Println("BookingRepository.BookRoom(): error occured during Bookinging process. Passed data: ", booking)
+		log.Println(err)
+		return models.Booking{}, err
+	}
+
+	return booking, nil
 }
