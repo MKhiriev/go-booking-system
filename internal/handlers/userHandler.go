b1f0c56b07ebcf0d61bb45504009303426485e33
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-04-07 11:49:33 +0500

    UserHandler: - fixed errors - added details to error responses

--- internal/handlers/userHandler.go
+++ internal/handlers/userHandler.go
@@ -12,11 +12,11 @@ import (
 
 func (h *Handlers) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var userParams models.User
-	userDecoder := json.NewDecoder(r.Body)
-	err := userDecoder.Decode(&userParams)
+	// convert JSON to models.User type
+	err := json.NewDecoder(r.Body).Decode(&userParams)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, err.Error())
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.CreateUser(): cannot convert JSON to models.User struct", err.Error())
 		return
 	}
 
@@ -24,47 +24,55 @@ func (h *Handlers) CreateUser(w http.ResponseWriter, r *http.Request) {
 	validator := NewUserValidator(&userParams)
 	if validator.AllUserFieldsValid != true {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, "Wrong user data provided", validator)
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.CreateUser(): User data is not valid!", validator)
 		return
 	}
 
+	// create user
 	createdUser, err := h.service.UserService.Create(userParams)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusInternalServerError, "Error occured during User creation")
+		pkg.ErrorResponse(w, http.StatusInternalServerError, "UserService.Create(): error occured", err.Error())
 		return
 	}
+
+	// return created user
 	pkg.Response(w, createdUser)
 }
 
 func (h *Handlers) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	// get all users
 	users := h.service.UserService.GetAll()
 
+	// convert all users to JSON
 	usersJSON, err := json.Marshal(users)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusInternalServerError, err.Error())
+		pkg.ErrorResponse(w, http.StatusInternalServerError, "UserHandler.GetAllUsers(): cannot convert slice of Users to JSON", err.Error())
 		return
 	}
 
+	// return all users
 	pkg.Response(w, usersJSON)
 }
 
 func (h *Handlers) GetUserById(w http.ResponseWriter, r *http.Request) {
-	// get user_id from path
+	// get user_id from query path
 	userIdStr := r.URL.Query().Get("user_id")
-	// convert string to int
+	// convert user_id param string to int
 	userId, err := strconv.Atoi(userIdStr)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, err.Error(), "user_id is not an integer!")
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.GetUserById(): user_id should be an integer!", err.Error())
+		return
 	}
 
 	// get User from services
 	user, err := h.service.UserService.GetUserById(userId)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, err.Error(), "UserService.GetUserById(): error occured")
+		pkg.ErrorResponse(w, http.StatusInternalServerError, "UserService.GetUserById(): error occured", err.Error())
+		return
 	}
 
 	// return found user
@@ -77,7 +85,7 @@ func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&userParamsToUpdate)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, err.Error())
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.UpdateUser(): cannot convert JSON to models.User struct", err.Error())
 		return
 	}
 
@@ -85,7 +93,7 @@ func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	validator := NewUserValidator(&userParamsToUpdate)
 	if validator.AllUserFieldsValid != true {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, "Wrong user data provided", validator)
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.UpdateUser(): User data is not valid!", validator)
 		return
 	}
 
@@ -93,7 +101,8 @@ func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	updatedUser, err := h.service.UserService.Update(userParamsToUpdate)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, http.StatusBadRequest, err.Error(), "UserService.GetUserById(): error occured")
+		pkg.ErrorResponse(w, http.StatusInternalServerError, "UserService.Update(): error occured", err.Error())
+		return
 	}
 
 	// return updated user
@@ -101,18 +110,25 @@ func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	// get user_id from query path
 	id := r.URL.Query().Get("user_id")
+	// convert user_id param string to int
 	userId, err := strconv.Atoi(id)
 	if err != nil {
-		pkg.ErrorResponse(w, 400, "только цифры")
+		log.Println(err)
+		pkg.ErrorResponse(w, http.StatusBadRequest, "UserHandler.DeleteUser(): user_id should be an integer!")
 		return
 	}
+
+	// delete user
 	_, err = h.service.UserService.Delete(userId)
 	if err != nil {
 		log.Println(err)
-		pkg.ErrorResponse(w, 500, "sorry")
+		pkg.ErrorResponse(w, http.StatusInternalServerError, "UserService.Delete(): error occured", err.Error())
 		return
 	}
+
+	// return success message
 	pkg.Response(w, "success")
 }
 
@@ -129,9 +145,9 @@ type UserValidator struct {
 func NewUserValidator(user *models.User) *UserValidator {
 	validationErrors := map[string]string{
 		"name_error":      "User.Name: should not be empty string",
-		"roleid_error":    "User.Role: should not be negative integer or zero",
+		"roleid_error":    "User.RoleId: should not be negative integer or zero",
 		"email_error":     "User.Email: wrong email format",
-		"telephone_error": "User.Name: wrong telephone number",
+		"telephone_error": "User.Telephone: wrong telephone number",
 	}
 
 	validator := &UserValidator{UserToValidate: user, ValidationErrors: validationErrors, AllUserFieldsValid: false}
@@ -154,18 +170,18 @@ func (u *UserValidator) ValidateFields() {
 
 	if u.UserToValidate.Name != "" {
 		u.IsNameValid = true
-		delete(u.ValidationErrors, "nameError")
+		delete(u.ValidationErrors, "name_error")
 	}
 	if u.UserToValidate.RoleId > 0 {
 		u.IsRoleIdValid = true
-		delete(u.ValidationErrors, "roleIdError")
+		delete(u.ValidationErrors, "roleid_error")
 	}
 	if emailRegex.MatchString(u.UserToValidate.Email) {
 		u.IsEmailValid = true
-		delete(u.ValidationErrors, "emailError")
+		delete(u.ValidationErrors, "email_error")
 	}
 	if telephoneRegex.MatchString(u.UserToValidate.Telephone) {
 		u.IsTelephoneValid = true
-		delete(u.ValidationErrors, "telephoneError")
+		delete(u.ValidationErrors, "telephone_error")
 	}
 }
