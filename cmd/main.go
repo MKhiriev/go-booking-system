40de30feabf7936b2c8796e8935f746adcad4532
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-04-11 16:24:17 +0500

    Minor fixes

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,12 @@ import (
 	"github.com/spf13/viper"
 	"humoBooking/internal/database"
 	"humoBooking/internal/handlers"
+	"humoBooking/internal/server"
 	"humoBooking/internal/services"
 	"log"
 )
 
-// main TODO add viper and godotenv
+// TODO добавить JWT токен!!! jwt.CustomClaims
 func main() {
 	if err := InitConfig(); err != nil {
 		log.Fatal("InitConfig(): error loading config.yaml", err)
@@ -21,11 +22,19 @@ func main() {
 	repository := database.NewDatabase(conn)
 	service := services.NewService(repository)
 	handler := handlers.NewHandler(service)
-	log.Println(handler.Init())
+	myServer := new(server.Server)
+	err := myServer.ServerRun(handler.Init(), viper.GetString("server.port"))
+	check(err)
 }
 
 func InitConfig() error {
 	viper.AddConfigPath("internal/configs")
-	viper.SetConfigName("config") // расширение не надо указывать
+	viper.SetConfigName("config")
 	return viper.ReadInConfig()
+}
+
+func check(err error) {
+	if err != nil {
+		log.Println(err)
+	}
 }
